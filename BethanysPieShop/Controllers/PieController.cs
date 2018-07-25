14c39197c07e480ae9b94391e790df38325d9aba
Dilip <dilip.geevarghese@cabotsolutions.com>
2018-07-25 09:10:35 +0530

    Added PieController, Models and View
    Added PieController, Models and View

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        public readonly IPieRepository _pieRepository;
+        public readonly ICategoryRepository _categoryRepository;
+        
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ActionResult List()
+        {
+            PiesListViewModel viewModel = new PiesListViewModel();
+            viewModel.Pies = _pieRepository.Pies;
+            viewModel.CurrentCategary = "Cheese Cakes";
+            return View(viewModel);
+        }
+    }
+}
